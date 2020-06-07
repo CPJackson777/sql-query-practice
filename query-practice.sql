713af97cd87640be2cf3c85e2da1b0e01e924249
Charles Jackson <cpjackson572@gmail.com>
2020-06-06 22:26:49 -0500

    added unfinished query

--- query-practice.sql
+++ query-practice.sql
@@ -80,3 +80,9 @@ WHERE Country != 'USA';
 SELECT FirstName, LastName, Customerid, Country
 FROM Customer
 WHERE Country == 'Brazil';
+
+-- brazil_customers_invoices.sql: Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
+
+SELECT c.FirstName, c.LastName, i.InvoiceId, i.BillingCountry, i.InvoiceDate
+FROM Invoice i 
+JOIN Customer c
